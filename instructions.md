f31bef9aeb43f957320fb4d463ce6cd775395887
Lucie Lénertová <lenertova.lucie@gmail.com>
2022-08-07 09:09:50 +0000

    Initial commit

---
+++ instructions.md
@@ -0,0 +1,16 @@
+## Podmínky
+
+### Úkol - Přidej svůj kód!!
+
+- nad první if podmínku tedy (řádek 6), přidejte další if podmínku
+- například můžete vytisknout uživateli "Maybe next time!", pokud zadá do konzole hodnotu 2 
+
+### Kód nefunguje? 
+- nechybí ti dvojtečka za if a else?
+- máš správně odsazený příkaz v podmínce (pomocí TAB)?
+- máš dvojité rovná se (==) v podmínce?
+- hledáš výstup programu v části Console ?
+- máš na začátku komentáře dvojkřížek (#) ?
+- nechybí ti uvozovky okolo řetězce či závorky za funkcí?
+- máš názvy funkcí malými písmeny?
+- máš víceslovný název proměnné oddělen podtržítkem?
---
+++ main.py
@@ -0,0 +1,32 @@
+# Spusťte program a odpovězte na otázky níže!
+
+number = 5
+print("I have thought of a number between 1 and 10")
+guess = int(input("Can you guess what it is?"))
+
+if guess == number:
+    print("Correct!")
+else:
+    print("Not correct!")
+
+print(guess == number)
+
+##### Úkol - Prozkoumej kód
+
+# Jakou hodnotu musí uživatel zadat do konzole, aby se vytiskl řetězec "Correct" do konzole?
+# Odpověď:
+
+# Co se stane, pokud uživatel zadá do konzole hodnotu 9 nebo 1 nebo -5?
+# Odpověď:
+
+# Co vytiskne do konzole příkaz na řádku 12? Jakého datového typu je hodnota?
+# Odpověď:
+
+# Co by se stalo, pokud bych na řádku 8 zapomněla odsadit print příkaz?
+# Odpověď:
+
+# Co se stane, pokud zapomenu na řádku 7 zapsat dvojtečku za podmínku? Vyzkoušejte! Nezapomeňte si pak kód opravit!
+# Odpověď:
+
+# Co se stane, pokud na řádku 7 zapíši = místo == ?Vyzkoušejte! Nezapomeňte si pak kód opravit!
+# Odpověď:
